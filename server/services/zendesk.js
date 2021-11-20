444009efbc4f441c2983aae83cf07f0bf683f9ee
nedjulius <julius.nedzinskas@gmail.com>
2021-11-20 18:40:20 -0500

    Add zendesk service

---
+++ server/services/zendesk.js
@@ -0,0 +1,20 @@
+const {PAGE_SIZE} = require('../constants/zendesk');
+const {requestService} = require('./request');
+
+class ZendeskService {
+  async getTicketList(page) {
+    const data = await requestService().get(`/api/v2/tickets?per_page=${PAGE_SIZE}&page=${page}`);
+
+    return {tickets: data.tickets, count: data.count}
+  }
+
+  async getTicket(id) {
+    const data = await requestService().get(`/api/v2/tickets/${id}`);
+
+    return data.ticket;
+  }
+}
+
+module.exports = {
+  zendeskService: () => new ZendeskService(),
+}
