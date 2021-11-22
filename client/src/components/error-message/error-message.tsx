717a29cff26a081f387e9abd0ca59d3c8dad9762
nedjulius <julius.nedzinskas@gmail.com>
2021-11-21 21:32:28 -0500

    Add error message component

---
+++ client/src/components/error-message/error-message.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './error-message.module.css';
+
+export const ErrorMessage: React.FC = () => (
+  <div className={styles.errorMessage}>An unexpected error occurred!</div>
+)
