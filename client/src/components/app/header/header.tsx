698223d2388e0b18399e1c4b20c39bb24b58cc90
nedjulius <julius.nedzinskas@gmail.com>
2021-11-21 22:44:41 -0500

    Add link to header logo

--- client/src/components/app/header/header.tsx
+++ client/src/components/app/header/header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './header.module.css';
 
 export const Header: React.FC = () => (
   <header className={styles.headerStrip}>
-    <h1 className={styles.heading}>Zendesk Ticket Viewer</h1>
+    <Link to="/"><h1 className={styles.heading}>Zendesk Ticket Viewer</h1></Link>
   </header>
 );
